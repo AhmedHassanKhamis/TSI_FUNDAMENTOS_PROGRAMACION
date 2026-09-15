a5a75f39eeb99b539c12a21c5cb71b3c5cbc5187
AhmedHassanKhamis <ahmedmario@hotmail.es>
2026-09-15 22:12:25 +0200

    arreglamos el fallo en el de factorial

--- actividades_ppt/actividad_3_5.c
+++ actividades_ppt/actividad_3_5.c
@@ -1,16 +1,42 @@
 #include <stdio.h>
 
+// version un poco mas larga
+// int main()
+// {
+//     int num,resultado = 1;5
+//     do{
+//         printf("Introduce un numero:\n");
+//         scanf("%d", &num);
+//         if (num > 0)
+//         {
+//             for (int i = num; i >= 1; i--)
+//             {
+//                 resultado*=i;
+//             }
+//             printf("El factorial de %d es: %d\n",num, resultado);
+//         }
+//         resultado = 1; 
+//     }while (num > 0);
+//     return 0;
+// }
+
+// version optimizada aunque lo dudo
 int main()
 {
-    int num,resultado;
-    do{
+    // a partir del 16 ya no funciona(no cabe el resultado en int) y se tendría que usar un long
+    int num = 0,resultado = 1;
+    while (num >= 0){
         printf("Introduce un numero:\n");
         scanf("%d", &num);
-        for (int i = num; i >= 1; i--)
+        if (num > 0)
         {
-            resultado*=i;
-        }   
-        printf("El factorial de %d es: %d\n",num, resultado);
-    }while (num > 0);
+            for (int i = num; i >= 1; i--)
+            {
+                resultado*=i;
+            }
+            printf("El factorial de %d es: %d\n",num, resultado);
+        }
+        resultado = 1; 
+    }
     return 0;
 }
